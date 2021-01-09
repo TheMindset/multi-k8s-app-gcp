32464d0281af36b460c8f6bfb159282933f66cbe
Guillaume MULEZI <guillaume.mulezi@carizy.com>
2021-01-09 08:47:18 +0100

    Fix deployment error #2

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ docker push themindset/multi-server:$SHA
 docker push themindset/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=themindset/multi-server:$SHA
-kubectl set image deployments/client-deployment client=themindset/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=themindset/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
